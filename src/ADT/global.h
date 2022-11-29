7001880dd2100bea5da655124d8631ce89278cb3
hitsutakarin77@gmail.com <hitsutakarin77@gmail.com>
2022-11-29 20:45:52 +0700

    Global variable

---
+++ src/ADT/global.h
@@ -0,0 +1,14 @@
+#ifndef __GLOBAL__
+#define __GLOBAL__
+
+#include "stack/stack.h"
+#include "set&map/map.h"
+
+extern Stack2 History;
+extern Map scRNG;
+extern Map scDiner;
+extern Map scHangman;
+extern Map scTOH;
+extern Map scSnake;
+
+#endif // !__GLOBAL__
