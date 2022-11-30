0323ef10fca89d6845ccd962f8835029502f16c6
Janetta Olivia <103167367+olviajntta@users.noreply.github.com>
2022-11-30 21:28:57 +0700

    Create history.h

---
+++ src/fungsi/history/history.h
@@ -0,0 +1,10 @@
+#ifndef __HISTORY_H
+#define __HISTORY_H
+#include "../../ADT/stack/stack.h"
+#include"../../ADT/word/word.h"
+
+void HISTORY(Stack stackhistory, int n);
+// HISTORY <n> menampilkan history permainan yang dimainkan
+// n adalah jumlah permainan yang telah dimankan yang ingin ditampilkan
+
+#endif
